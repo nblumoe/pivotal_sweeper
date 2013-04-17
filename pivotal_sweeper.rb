32ca8c704f63e01915455181f424ad67b66e0fdb
Nils Blum-Oeste <nblumoeste@googlemail.com>
2013-04-17 09:03:03 +0200

    Actually exlude some hardcoded branches

--- pivotal_sweeper.rb
+++ pivotal_sweeper.rb
@@ -10,17 +10,16 @@ project = PivotalTracker::Project.find(PROJECT_ID)
 
 Dir.chdir(GIT_DIR)
 `git branch -r --no-merged`.each_line do |raw_branch|
-  branch = raw_branch.gsub("origin/", "").chomp
-  if branch == 'prestage' || branch == 'staging'
-    return
-  end
-  puts "Branch: #{branch}"
-  project.stories.all(:label => branch, :includedone => true).each do |story|
-    puts "  Story: #{story.name} - #{story.current_state.upcase}"
-    puts "    Requested by: #{story.requested_by}"
-    puts "    Owned by: #{story.owned_by}"
-    puts "    URL: #{story.url}"
-    puts ""
+  branch = raw_branch.gsub("origin/", "").chomp().strip()
+  unless ['prestage', 'staging', 'production'].include? branch
+    puts "Branch: #{branch}"
+    project.stories.all(:label => branch, :includedone => true).each do |story|
+      puts "  Story: #{story.name} - #{story.current_state.upcase}"
+      puts "    Requested by: #{story.requested_by}"
+      puts "    Owned by: #{story.owned_by}"
+      puts "    URL: #{story.url}"
+      puts ""
+    end
   end
   puts ""
 end
