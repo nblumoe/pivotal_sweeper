80a60e9d2f1337cf34b7e1ec1d4eb9feee468d48
Nils Blum-Oeste <nblumoeste@googlemail.com>
2013-04-17 08:36:41 +0200

    Added script to delete merged branches on origin

---
+++ branch_sweeper.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require './config'
+
+persistent_branches = ["master", "staging", "prestage", "HEAD -> master", "* master"]
+
+Dir.chdir(GIT_DIR)
+`git branch -r --merged`.each_line do |remote_branch|
+  branch = remote_branch.gsub("origin/", "").chomp.strip
+  unless persistent_branches.include?( branch )
+    puts "git push origin :#{branch}"
+  end
+end
+#`git branch --merged`.each_line do |branch|
+#  branch = branch.chomp.strip
+#  unless persistent_branches.include?( branch )
+#    `git br -d #{branch}`
+#  end
+#end
